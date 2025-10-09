224a798ebfe5e86ded2466f0a2951cf6c7e9837d
GabrielLucas <gabriellucas3301@gmail.com>
2025-10-09 16:14:11 -0300

    migration bugs

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CommentController extends Controller
+
+{
+    public function store(Request $request, Post $post)
+    {
+        $data = $request->validate([
+            'body' => 'required|string|max:2000',
+        ]);
+
+        $data['user_id'] = Auth::id();
+        $data['post_id'] = $post->id;
+
+        Comment::create($data);
+
+        return redirect()->route('posts.show', $post)->with('success', 'Comentário adicionado');
+    }
+
+    public function destroy(Comment $comment)
+    {
+        $user = Auth::user();
+        if ($user->id !== $comment->user_id && !$user->is_admin && $user->id !== $comment->post->user_id) {
+            abort(403);
+        }
+
+        $comment->delete();
+        return back()->with('success', 'Comentário removido');
+    }
+
+    public function edit(Comment $comment)
+    {
+        if (Auth::id() !== $comment->user_id && !Auth::user()->is_admin) {
+            abort(403);
+        }
+        return view('comments.edit', compact('comment'));
+    }
+
+    public function update(Request $request, Comment $comment )
+    {
+        if (Auth::id() !== $comment->user_id && !Auth::user()->is_admin) abort(403);
+
+        $data = $request->validate([
+
+            'body' => 'required|string|max:200',
+        ]);
+
+        $comment->update($data);
+        return redirect()->route('posts.show', $comment->post_id)->with('success', 'Post atualizado');
+    }
+
+}
---
+++ database/migrations/2025_10_07_183057_create_roles.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        if(Schema::hasTable("roles")) {
+            Schema::create('role', function (Blueprint $table) {
+                $table->id();
+                $table->string('name')->unique();
+                $table->string('description')->nullable();
+                $table->timestamps();
+            });
+        } 
+
+        if(Schema::hasTable("permissions")) {
+            Schema::create("permission", function (Blueprint $table) {
+                $table->id();
+                $table->string('name')->unique();
+                $table->string('description')->nullable();
+                $table->timestamps();
+            });
+        }
+
+        if(Schema::hasTable("role_permission")) {
+            Schema::create("role_permission", function (Blueprint $table) {
+                $table->id();
+                $table->foreignId("role_id")->constrained("role")->cascadeOnDelete();
+                $table->foreignId("permission_id")->constrained("permission")->cascadeOnDelete();
+                $table->timestamps();
+            });
+        }
+    }
+
+        public function down(): void
+    {
+        Schema::dropIfExists('role');
+        Schema::dropIfExists('permission');
+        Schema::dropIfExists('role_permission');
+    }
+    };
